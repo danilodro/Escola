0d582be3ec1d74ee40f678b848ed81ffd0274d99
Danilo Oliveira <danilo.oliveira@infracommerce.com.br>
2021-09-30 17:42:05 -0300

    python backend Django Rest Framework

---
+++ setup/urls.py
@@ -0,0 +1,16 @@
+from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
+router.register('Matricula', MatriculaViewSet, basename='Matriculas')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('alunos/<int:pk>/matriculas/',ListaMatriculasAluno.as_view()),
+    path('cursos/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view())
+]
